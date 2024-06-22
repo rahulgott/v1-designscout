7e7bc27cf697cd95aa3de6785885ab0d140fc36d
rahulgott <rahul.gott@gmail.com>
2024-06-21 19:20:48 -0700

    Updating page title, fix attempt-3

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,7 @@ import TestContainer from './components/playground/TestContainer'
 function App() {
 
   return (
-    <div style={{
-      height: "100svh",
-      backgroundImage: `url("src/assets/background-dots.png")`,
-      backgroundRepeat: "repeat",
-      backgroundSize: "50px",
-      boxShadow: "inset 0 0 0 1000px rgba(0, 0, 0, 0.2)",
-      }}>
+    <div style={{height: "100svh"}}>
       <TopNav />
       <TestContainer />
     </div>
