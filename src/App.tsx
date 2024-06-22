e838078f83433974210882dcbc113ba0866da62c
rahulgott <rahul.gott@gmail.com>
2024-06-21 19:12:00 -0700

    attempt to fix ntwlify bg-image issue-2

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ import TestContainer from './components/playground/TestContainer'
 function App() {
 
   return (
-    <div style={{height: "100svh"}}>
+    <div style={{
+      height: "100svh",
+      backgroundImage: `url("src/assets/background-dots.png")`,
+      backgroundRepeat: "repeat",
+      backgroundSize: "50px",
+      boxShadow: "inset 0 0 0 1000px rgba(0, 0, 0, 0.2)",
+      }}>
       <TopNav />
       <TestContainer />
     </div>
